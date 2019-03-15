ee36e17a858f19fe1d0469b0941cdd12359e6819
aliefbagja <36681314+aliefbagja@users.noreply.github.com>
2019-03-16 03:43:23 +0700

    Update izin.php

--- izin.php
+++ izin.php
@@ -2,7 +2,7 @@
 $domainuing = array('www.bosball.com', 'www.skytivi.com', 'www.suntivi.com', 'www.noztv.com', 'www.tvsakeena.com', 'www.tvkicks.com', 'www.bisatv.com', 'www.soniktv.com', 'www.tvjava.com', 'www.tvfifa.com', 'www.tvball7.com', 'www.tvball7.xyz', 'www.indiostv.com',  'www.livebal.com', 'www.tvanda.com', 'www.shootgol.net', 'www.boomingtv.com', 'www.tvronaldo.com', 'www.tvnasional.com', );
 if (!isset($_SERVER['HTTP_REFERER']))
 {
-	header("Location: http://www.lenztv.com/");
+	header("Location: http://www.lenzatv.com/2018/09/bein-1.html");
 die();
 }
 else if (isset($_SERVER['HTTP_REFERER']))
@@ -10,7 +10,7 @@ else if (isset($_SERVER['HTTP_REFERER']))
 	$izinkan=parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
 	if (!in_array($izinkan, $domainuing))
 	{
-		header("Location: http://www.lenztv.com/");
+		header("Location: http://www.lenzatv.com/2018/09/bein-1.html");
 die();
 	}
 }
