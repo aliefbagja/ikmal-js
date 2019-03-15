bf9ec7fc09d49e0e80198f60feed921daec83e4a
aliefbagja <36681314+aliefbagja@users.noreply.github.com>
2019-03-16 04:54:34 +0700

    Update and rename izin.php to 1zin.php

---
+++ 1zin.php
@@ -0,0 +1,17 @@
+<?php
+$domainuing = array('www.noscdn.com', 'www.skytivi.com', 'www.suntivi.com', 'www.noztv.com', 'www.tvsakeena.com', 'www.tvkicks.com', 'www.bisatv.com', 'www.soniktv.com', 'www.tvjava.com', 'www.tvfifa.com', 'www.tvball7.com', 'www.tvball7.xyz', 'www.indiostv.com',  'www.livebal.com', 'www.tvanda.com', 'www.shootgol.net', 'www.boomingtv.com', 'www.tvronaldo.com', 'www.tvnasional.com', );
+if (!isset($_SERVER['HTTP_REFERER']))
+{
+	header("Location: http://www.lenzatv.com/");
+die();
+}
+else if (isset($_SERVER['HTTP_REFERER']))
+{
+	$izinkan=parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+	if (!in_array($izinkan, $domainuing))
+	{
+		header("Location: http://www.lenzatv.com/");
+die();
+	}
+}
+?>
