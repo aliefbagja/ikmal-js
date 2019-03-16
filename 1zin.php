637ca6564c2775fac17249590cf3d5f19a5cc80d
aliefbagja <36681314+aliefbagja@users.noreply.github.com>
2019-03-16 08:50:54 +0700

    Update 1zin.php

--- 1zin.php
+++ 1zin.php
@@ -1,4 +1,4 @@
-<?php
+<?php echo
 $domainuing = array('www.bosball.com', 'www.skytivi.com', 'www.suntivi.com', 'www.noztv.com', 'www.tvsakeena.com', 'www.tvkicks.com', 'www.bisatv.com', 'www.soniktv.com', 'www.tvjava.com', 'www.tvfifa.com', 'www.tvball7.com', 'www.tvball7.xyz', 'www.indiostv.com',  'www.livebal.com', 'www.tvanda.com', 'www.shootgol.net', 'www.boomingtv.com', 'www.tvronaldo.com', 'www.tvnasional.com', );
 if (!isset($_SERVER['HTTP_REFERER']))
 {
@@ -7,10 +7,10 @@ die();
 }
 else if (isset($_SERVER['HTTP_REFERER']))
 {
-	$izinkan=parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+	echo $izinkan=parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
 	if (!in_array($izinkan, $domainuing))
 	{
 		header("Location: http://www.lenzatv.com/");
-die();
-	}
+die();	}
 }
+?>
