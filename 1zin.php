a4b6c2ea3825764a4dbcb7b6e0b43460769a264f
aliefbagja <36681314+aliefbagja@users.noreply.github.com>
2019-03-16 09:51:20 +0700

    Update 1zin.php

--- 1zin.php
+++ 1zin.php
@@ -1,16 +1,17 @@
-<? php
-echo '$domainuing = array('www.bosball.com', 'www.skytivi.com', 'www.suntivi.com', 'www.noztv.com', 'www.tvsakeena.com', 'www.tvkicks.com', 'www.bisatv.com', 'www.soniktv.com', 'www.tvjava.com', 'www.tvfifa.com', 'www.tvball7.com', 'www.tvball7.xyz', 'www.indiostv.com',  'www.livebal.com', 'www.tvanda.com', 'www.shootgol.net', 'www.boomingtv.com', 'www.tvronaldo.com', 'www.tvnasional.com', );';
-echo 'if (!isset($_SERVER['HTTP_REFERER']))';
-echo '{';
-echo 'header("Location: http://www.lenzatv.com/");';
-echo 'die();';
-echo '}';
-echo 'else if (isset($_SERVER['HTTP_REFERER']))';
-echo '{';
-echo '$1zin=parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);';
-echo 'if (!in_array($1zin, $domainuing))';
-echo '{';
-echo 'header("Location: http://www.lenzatv.com/");';
-echo 'die();	}';
-echo '}';
+<?php
+$domainuing = array('www.noscdn.com', 'www.skytivi.com', 'www.suntivi.com', 'www.noztv.com', 'www.tvsakeena.com', 'www.tvkicks.com', 'www.bisatv.com', 'www.soniktv.com', 'www.tvjava.com', 'www.tvfifa.com', 'www.tvball7.com', 'www.tvball7.xyz', 'www.indiostv.com',  'www.livebal.com', 'www.tvanda.com', 'www.shootgol.net', 'www.boomingtv.com', 'www.tvronaldo.com', 'www.tvnasional.com', );
+if (!isset($_SERVER['HTTP_REFERER']))
+{
+	header("Location: http://www.wouwza.com/");
+die();
+}
+else if (isset($_SERVER['HTTP_REFERER']))
+{
+	$izinkan=parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+	if (!in_array($izinkan, $domainuing))
+	{
+		header("Location: http://www.wouwza.com/");
+die();
+	}
+}
 ?>
